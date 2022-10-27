06195fb21b627176763352dcb0f7f16af88c182c
jaymbans <88808561+jaymbans@users.noreply.github.com>
2022-10-27 02:26:42 -0400

    added ootb workouts

---
+++ src/workouts/workouts.js
@@ -0,0 +1,342 @@
+// collection of OOTB and Custom workouts
+
+export const ootbWorkouts = {
+  data: [
+    {
+      workoutName: 'bench press',
+      muscleGroup: 'chest',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'shoulder press',
+      muscleGroup: 'shoulders',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'tricep extension',
+      muscleGroup: 'triceps',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'dips',
+      muscleGroup: 'triceps',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'pushups',
+      muscleGroup: 'chest',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'side/lateral raises',
+      muscleGroup: 'shoulders',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'squat',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'lunges',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'bulgarian split squats',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'calf raises',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'hack squats',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'leg extension',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'deadlifts',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'hamstring curls',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'romanian deadlifts',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'pullups',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'supermans',
+      muscleGroup: 'back',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'close-grip benchpress',
+      muscleGroup: 'triceps',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'french press',
+      muscleGroup: 'triceps',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'cable side raises',
+      muscleGroup: 'shoulders',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'overhead tricep extension',
+      muscleGroup: 'triceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'facepulls',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'front squats',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'leg press',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'hex press',
+      muscleGroup: 'chest',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'dumbbell flies',
+      muscleGroup: 'chest',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'dumbbell kickbacks',
+      muscleGroup: 'triceps',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'dumbbell rows',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'lat pull down',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'T-bar row',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'shrugs',
+      muscleGroup: 'shoulders',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'smith squats',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'good mornings',
+      muscleGroup: 'back',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'pull-throughs',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'preacher curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'concentration curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'incline curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'reverse curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'cable curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'machine curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'incline bench press',
+      muscleGroup: 'chest',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'arnold press',
+      muscleGroup: 'shoulders',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'reverse peck deck',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'front raises',
+      muscleGroup: 'shoulders',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'deficit deadlifts',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'rack pulls',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'kettlebell swings',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'pistol squat',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'pedlay row',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'high row',
+      muscleGroup: 'back',
+      split: 'pull',
+      weight: null
+    },
+    {
+      workoutName: 'hammer curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'box squat',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'decline press',
+      muscleGroup: 'chest',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'shoulder press machine',
+      muscleGroup: 'shoulders',
+      split: 'push',
+      weight: null
+    },
+    {
+      workoutName: 'wrist curls',
+      muscleGroup: 'biceps',
+      split: null,
+      weight: null
+    },
+    {
+      workoutName: 'glute bridges',
+      muscleGroup: 'legs',
+      split: null,
+      weight: null
+    },
+  ]
+}
